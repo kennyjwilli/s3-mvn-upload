20673fc642765c2679a77e9b7cd2273ecc8f3cc2
Kenny Williams <kennyjwilli@gmail.com>
2020-07-26 11:07:45 -0700

    Upload jar pom.xml

--- src/s3_mvn_upload/core.clj
+++ src/s3_mvn_upload/core.clj
@@ -1,7 +1,10 @@
 (ns s3-mvn-upload.core
   (:require
     [clojure.string :as str]
-    [clojure.java.shell :as shell]))
+    [clojure.java.io :as io]
+    [clojure.java.shell :as shell])
+  (:import (java.util.jar JarFile JarEntry JarInputStream)
+           (java.io IOException)))
 
 (defn print-err
   [x]
@@ -14,14 +17,18 @@
     artifact-name
     (symbol artifact-name artifact-name)))
 
+(defn jar-name
+  [x]
+  (first (str/split x #"\.jar")))
+
 (defn s3-key
-  [artifact-name version]
+  [artifact-name version extension]
   (let [n (canonicalize-name artifact-name)]
     (str/join "/"
               (concat (str/split (namespace n) #"\.")
                       [(name n)
                        version
-                       (str (name n) "-" version ".jar")]))))
+                       (str (name n) "-" version extension)]))))
 
 (defn md5sum
   [path]
@@ -33,6 +40,24 @@
     (catch Exception ex
       (print-err (.getMessage ex))
       nil)))
+
+(defn find-pom-contents
+  "Find path of pom file in jar file, or nil if it doesn't exist"
+  [^JarFile jar-file]
+  (try
+    (loop [[^JarEntry entry & entries] (enumeration-seq (.entries jar-file))]
+      (when entry
+        (let [name (.getName entry)]
+          (if (and (str/starts-with? name "META-INF/")
+                   (str/ends-with? name "pom.xml"))
+            (with-open [jis (.getInputStream jar-file entry)]
+              (slurp jis))
+            (recur entries)))))
+    (catch IOException _t nil)))
+
+(comment
+  (find-pom (JarFile. (io/file "s3-mvn-upload.jar")))
+  )
 
 (defn upload!
   [path s3-uri]
@@ -48,11 +73,20 @@
   (assert jar-file "missing jar file")
 
   (let [md5-path (str jar-file ".md5")
-        s3-key (s3-key (clojure.edn/read-string artifact-name-str) version)
-        urif #(str uri-start "/" %)]
+        pom-path (str (jar-name jar-file) ".pom")
+        pom-md5-path (str (jar-name jar-file) ".pom.md5")
+        artifact-name (clojure.edn/read-string artifact-name-str)
+        urif #(str uri-start "/" %)
+        pom-contents (find-pom-contents (JarFile. (io/file jar-file)))]
+    (assert pom-contents "Jar is missing a pom.xml file.")
+
     (spit md5-path (md5sum jar-file))
-    (upload! md5-path (urif (str s3-key ".md5")))
-    (upload! jar-file (urif s3-key))))
+    (spit pom-path pom-contents)
+    (spit pom-md5-path (md5sum pom-path))
+    (upload! md5-path (urif (s3-key artifact-name version ".jar.md5")))
+    (upload! pom-path (urif (s3-key artifact-name version ".pom")))
+    (upload! pom-md5-path (urif (s3-key artifact-name version ".pom.md5")))
+    (upload! jar-file (urif (s3-key artifact-name version ".jar")))))
 
 (defn -main
   [& args]
